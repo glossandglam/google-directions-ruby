9f00efd66ee91bd833eed96ea6b4db9a9f9a54a0
jer <deathbyjer@gmail.com>
2015-05-06 02:54:30 -0400

    allow options to fill in the rest

--- lib/google_directions.rb
+++ lib/google_directions.rb
@@ -17,10 +17,10 @@ class GoogleDirections
     :mode => :driving,
   }
 
-  def initialize(origin, destination, opts=@@default_options)
+  def initialize(origin, destination, opts={})
     @origin = origin
     @destination = destination
-    @options = opts.merge({:origin => @origin, :destination => @destination})
+    @options = @@default_options.merge({:origin => @origin, :destination => @destination})
 
     @url = @@base_url + '?' + @options.to_query
     @xml = open(@url).read
